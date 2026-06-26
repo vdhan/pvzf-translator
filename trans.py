8f2589ef75f8d297a41421f529f2a7a8e39fa641
An <anvudh@gmail.com>
2026-06-26 22:39:52 +0700

    phan he Strings

--- trans.py
+++ trans.py
@@ -1,6 +1,8 @@
 import json
 import sys
+
 from argparse import ArgumentParser, RawDescriptionHelpFormatter, Namespace
+from json import JSONDecodeError
 from pathlib import Path
 
 
@@ -26,27 +28,72 @@ def hop_nhat_ds(a: list, b: list, khoa: str) -> list:
     return list(tam.values())
 
 
-def hop_nhat_dict(a: dict, b: dict) -> dict:
+def hop_nhat_dict(a: dict, b: dict, khoa: str) -> dict:
+    """
+    Hợp nhất từ điển con
+    """
+    # nếu từ khóa trong từ điển đích không phải kiểu dữ liệu từ điển thì lấy từ khóa của từ điển nguồn
+    if not isinstance(b[khoa], dict):
+        return a[khoa]
+
+    tam: dict = {}
+    khoa_moi: list = []
+    khoa_cu: list = []
+    for khoa_con in b[khoa]:
+        if khoa_con in a[khoa]:
+            tam[khoa_con] = b[khoa][khoa_con]
+        else:
+            khoa_cu.append(b[khoa])
+
+    for khoa_con in a[khoa]:
+        if khoa_con not in tam:
+            tam[khoa_con] = a[khoa][khoa_con]
+            khoa_moi.append(khoa_con)
+
+    if khoa_moi or khoa_cu:
+        print(f'  Từ khóa {khoa}')
+        if khoa_moi:
+            print('    Các từ khóa mới:')
+            for mon in khoa_moi:
+                print(f'    - {mon}')
+
+            print()
+
+        if khoa_cu:
+            print('    Các từ khóa cũ:')
+            for mon in khoa_cu:
+                print(f'    - {mon}')
+
+            print()
+
+
+def so_sanh_dict(a: dict, b: dict) -> dict:
     """
     Hợp nhất 2 từ điển, nếu có từ khóa trùng thì chọn cái sau (từ điển B)
     """
-    tam: dict = {}
+    c: dict = {}
     khoa_moi: list = []
-    for khoa in a:
-        if khoa in b:
-            tam[khoa] = a[khoa]
-
     for khoa in b:
-        if khoa not in tam:
-            tam[khoa] = b[khoa]
+        if khoa in a:
+            if isinstance(a[khoa], dict):
+                c[khoa] = hop_nhat_dict(a, b, khoa)
+
+            else:
+                c[khoa] = b[khoa]
+
+    for khoa in a:
+        if khoa not in c:
+            c[khoa] = a[khoa]
             khoa_moi.append(khoa)
 
     if khoa_moi:
         print('Các từ khóa mới:')
-        for khoa in khoa_moi:
-            print(f'- {khoa}')
+        for mon in khoa_moi:
+            print(f'- {mon!r}')
 
-    return tam
+        print()
+
+    return c
 
 
 def so_sanh_almanac() -> None:
@@ -65,8 +112,8 @@ def so_sanh_almanac() -> None:
     tep_moi: list = []
     ds_json: tuple = tuple(thu_muc_b.glob('*.json'))
     if ds_json:
-        almanac: str = f'{thu_muc_goc}/c/{phan_he}'
-        Path(almanac).mkdir(parents=True, exist_ok=True)
+        duong_dan: str = f'{thu_muc_goc}/c/{phan_he}'
+        Path(duong_dan).mkdir(parents=True, exist_ok=True)
         for mon in ds_json:
             tep_b: str = str(mon)
             tam: str = tep_b.rsplit('/', 1)
@@ -84,7 +131,7 @@ def so_sanh_almanac() -> None:
             with open(tep_b, encoding='utf-8') as f:
                 du_lieu_b: dict = json.load(f)
 
-            tep_c: str = f'{almanac}/{tep_json}'
+            tep_c: str = f'{duong_dan}/{tep_json}'
             print(f'+ {tep_c}')
             if tep_json == 'LawnStringsTranslate.json':
                 muc: str = 'plants'
@@ -99,7 +146,7 @@ def so_sanh_almanac() -> None:
                     json.dump({muc: du_lieu}, f, ensure_ascii=False, indent=4)
 
             else:
-                du_lieu: dict = hop_nhat_dict(du_lieu_b, du_lieu_a)
+                du_lieu: dict = so_sanh_dict(du_lieu_b, du_lieu_a)
                 with open(tep_c, 'w', encoding='utf-8') as f:
                     json.dump(du_lieu, f, ensure_ascii=False, indent=4)
 
@@ -126,9 +173,63 @@ def so_sanh_strings() -> None:
         print(f'Thư mục {thu_muc_b} không tồn tại')
         sys.exit(1)
 
-    """Từ khóa có 2 dạng: str và dict"""
+    tep_moi: list = []
+    tep_bom: list = []
+    ds_json: tuple = tuple(thu_muc_b.glob('*.json'))
+    if ds_json:
+        duong_dan: str = f'{thu_muc_goc}/c/{phan_he}'
+        Path(duong_dan).mkdir(parents=True, exist_ok=True)
+        for mon in ds_json:
+            tep_b: str = str(mon)
+            tam: str = tep_b.rsplit('/', 1)
+            tep_json: str = tam[1]
+            tep_a = Path(thu_muc_a, tep_json)
 
-    # TODO
+            # tệp có trong ngôn ngữ nguồn nhưng không có trong ngôn ngữ đích
+            if not tep_a.is_file():
+                tep_moi.append(tep_json)
+                continue
+
+            try:
+                with open(tep_a, encoding='utf-8') as f:
+                    du_lieu_a: dict = json.load(f)
+            except JSONDecodeError:
+                tep_bom.append(tep_a)
+                with open(tep_a, encoding='utf-8-sig') as f:
+                    du_lieu_a: dict = json.load(f)
+
+            try:
+                with open(tep_b, encoding='utf-8') as f:
+                    du_lieu_b: dict = json.load(f)
+            except JSONDecodeError:
+                tep_bom.append(tep_b)
+                with open(tep_b, encoding='utf-8-sig') as f:
+                    du_lieu_b: dict = json.load(f)
+
+            tep_c: str = f'{duong_dan}/{tep_json}'
+            print(f'+ {tep_c}')
+
+            du_lieu: dict = so_sanh_dict(du_lieu_b, du_lieu_a)
+            with open(tep_c, 'w', encoding='utf-8') as f:
+                json.dump(du_lieu, f, ensure_ascii=False, indent=4)
+
+            print()
+
+    if tep_moi:
+        print('Các tệp mới:')
+        for tep in tep_moi:
+            print(f'- {tep}')
+
+        print()
+
+    if tep_bom:
+        vang: str = '\x1b[1;33m'
+        dat_lai: str = '\x1b[0m'
+        print(f'{vang}Các tệp BOM:{dat_lai}')
+        for tep in tep_bom:
+            print(f'- {tep}')
+
+        print()
 
 
 if __name__ == '__main__':
